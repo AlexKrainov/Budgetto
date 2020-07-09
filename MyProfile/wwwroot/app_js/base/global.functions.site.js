bba30a952b97a5c994593ae7a760f118e1578131
Alex <akraynov@navicons.ru>
2020-07-09 18:35:58 +0300

    template list

--- MyProfile/wwwroot/app_js/base/global.functions.site.js
+++ MyProfile/wwwroot/app_js/base/global.functions.site.js
@@ -1,91 +1,90 @@
 ﻿String.prototype.replaceAll = function (search, replace) {
-	return this.split(search).join(replace);
+    return this.split(search).join(replace);
 }
 
 //send ajax by jquery , by default method send = "POST"
 function sendAjax(url, value = null, type = "POST") {
-	return $.ajax({
-		type: type,
-		url: url,
-		data: value ? JSON.stringify(value) : null,
-		contentType: 'application/json; charset=utf-8',
-		dataType: 'json',
-		success: function (response) {
-			return response;
-		},
-		error: function (xhr, status, error) {
-			console.log(error);
-		}
-	});
+    return $.ajax({
+        type: type,
+        url: url,
+        data: value ? JSON.stringify(value) : null,
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (response) {
+            return response;
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+        }
+    });
 }
 
 function parseQueryString() {
 
-	var str = window.location.search;
-	var objURL = {};
+    var str = window.location.search;
+    var objURL = {};
 
-	str.replace(
-		new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-		function ($0, $1, $2, $3) {
-			objURL[$1] = $3;
-		}
-	);
-	return objURL;
+    str.replace(
+        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+        function ($0, $1, $2, $3) {
+            objURL[$1] = $3;
+        }
+    );
+    return objURL;
 };
 
 //return string by 3 digits {### ### ###}
 function numberOfThreeDigits(str) {
-	return str ? str.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') : "";
+    return str ? str.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') : "";
 }
 
 function GetDateByFormat(date, format) {
-	if (date && moment(date).isValid()) {
-		return moment(date).format(format);
-	}
-	return "";
+    if (date && moment(date).isValid()) {
+        return moment(date).format(format);
+    }
+    return "";
 }
 
 
 var TemplateColumnTypeEnum = Object.freeze({
-	Undefined: 0,
-	BudgetSection: 1,
-	DaysForMonth: 2,
-	MonthsForYear: 3,
-	YearForYear: 4,
-	Percent: 5,
-	Comment: 6,
-	WeekForMonth: 7
+    Undefined: 0,
+    BudgetSection: 1,
+    DaysForMonth: 2,
+    MonthsForYear: 3,
+    YearForYear: 4,
+    Percent: 5,
+    Comment: 6,
+    WeekForMonth: 7
 });
 
 var FooterActionTypeEnum = Object.freeze({
-	Undefined: 0,
-	Sum: 1,
-	Avr: 2,
-	Min: 3,
-	Max: 4
+    Undefined: 0,
+    Sum: 1,
+    Avr: 2,
+    Min: 3,
+    Max: 4
 });
 
 var FormulaFieldTypeEnum = Object.freeze({
-	Undefined: 0,
-	Section: 1,
-	Number: 2,
-	Mark: 3,
-	Parentheses: 4,
-	Days: 5
+    Undefined: 0,
+    Section: 1,
+    Number: 2,
+    Mark: 3,
+    Parentheses: 4,
+    Days: 5
 });
 
 var PaletteColorPicker = ["#1abc99", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"];
 
 function GetRandomColor() {
-	return PaletteColorPicker[GetRandomInt(PaletteColorPicker.length)];
+    return PaletteColorPicker[GetRandomInt(PaletteColorPicker.length)];
 }
 
 function GetRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
-function ParseXml(xml, arrayTags)
-{
+function ParseXml(xml, arrayTags) {
     var dom = null;
     if (window.DOMParser) {
         dom = (new DOMParser()).parseFromString(xml, "text/xml");
@@ -165,3 +164,12 @@ function GetLinkForView(template) {
         return '/Budget/Index/' + template.id;
     }
 }
+
+function GetRusName(n, text_forms) {
+    n = Math.abs(n) % 100; var n1 = n % 10;
+    if (n > 10 && n < 20) { return text_forms[2]; }
+    if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+    if (n1 == 1) { return text_forms[0]; }
+    return text_forms[2];
+}
+
--- MyProfile/wwwroot/app_js/template-list.js
+++ MyProfile/wwwroot/app_js/template-list.js
@@ -1,45 +1,59 @@
 ﻿
 var TemplateListVue = new Vue({
-	el: "#template-list-vue",
-	data: {
-		templates: [],
-		activeTemplatePeriodTypeID: -1,
-		search: null,
-	},
-	watch: {
-		search: function (newValue, oldValue) {
-			if (!newValue) {
-				this.templates.forEach(function (el, index) { el.isShow = true; });
-			}
+    el: "#template-list-vue",
+    data: {
+        templates: [],
+        activeTemplatePeriodTypeID: -1,
+        search: null,
+        msnry: {},
+    },
+    watch: {
+        search: function (newValue, oldValue) {
+            if (!newValue) {
+                this.templates.forEach(function (el, index) { el.isShow = true; });
+            }
 
-			for (var i = 0; i < this.templates.length; i++) {
-				this.templates[i].isShow = this.templates[i].name.indexOf(newValue) >= 0;
-			}
-		}
-	},
-	mounted: function () {
-		this.init()
-			.then(function () {
-					
-			});
-	},
-	methods: {
-		init: function () {
+            for (var i = 0; i < this.templates.length; i++) {
+                this.templates[i].isShow = this.templates[i].name.indexOf(newValue) >= 0;
+            }
+        }
+    },
+    mounted: function () {
+        this.init()
+            .then(function () {
+                TemplateListVue.msnry = new Masonry('#templates', {
+                    itemSelector: '.template-card:not(.d-none)',
+                    columnWidth: '.template-card-sizer',
+                    originLeft: true,
+                    horizontalOrder: true
+                });
+            });
+    },
+    methods: {
+        init: function () {
 
-			return sendAjax("/Template/GetTemplates", null, "GET")
-				.then(function (result) {
-					if (result.isOk == true) {
-						TemplateListVue.templates = result.templates;
-					}
-				});
-		},
-		getLinkForView: function (template) {
-			return GetLinkForView(template);
-		},
-		getDateByFormat: function (date, format) {
-			return GetDateByFormat(date, format);
-		}
-	}
+            return sendAjax("/Template/GetTemplates", null, "GET")
+                .then(function (result) {
+                    if (result.isOk == true) {
+                        TemplateListVue.templates = result.templates;
+                    }
+                });
+        },
+        getLinkForView: function (template) {
+            return GetLinkForView(template);
+        },
+        getDateByFormat: function (date, format) {
+            return GetDateByFormat(date, format);
+        },
+        reload: function () {
+            setTimeout(function () {
+                TemplateListVue.msnry.layout();
+            }, 15)
+        },
+        getRusName: function (num) {
+            return GetRusName(num, ['Колонка', 'Колонки', 'Колонок']);
+        }
+    }
 });
 
 Vue.config.devtools = true;
